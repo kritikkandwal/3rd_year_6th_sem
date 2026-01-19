0f613db1530b0e3dc833bb2c4c74d7a463f261f6
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-01-19 15:55:34 +0530

    Add files via upload

---
+++ times.py
@@ -0,0 +1,6 @@
+# Define a tuple called 'exam_st_date' containing the exam start date in the format (day, month, year)
+new = (11, 12, 2014)
+
+# Print the exam start date using string formatting
+# The '%i' placeholders are filled with the values from the 'exam_st_date' tuple
+print("The examination will start from : %i / %i / %i" % new)
