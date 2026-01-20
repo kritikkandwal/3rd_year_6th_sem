0fb5046e75696fa297dfa3987fa5f405fa2c9d60
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-01-20 17:51:56 +0530

    Add files via upload

---
+++ replace the substring.py
@@ -0,0 +1,5 @@
+str = input("enter the string: ")
+old_substring = input("to replace: ")
+new_substring = input("to be replaced: ")
+replaced_str = str.replace(old_substring, new_substring)
+print(replaced_str)  # Output: "Hello, Universe!"
