79b7fec15c5ae6333cd05f2287dc78a293caaf72
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-02-03 20:49:15 +0530

    Add files via upload

---
+++ querryselector.js
@@ -0,0 +1,6 @@
+let firstel=document.querySelector("p");
+console.dir(firstel);
+let all=document.querySelectorAll("p");
+let adldl=document.querySelectorAll(".class_name");
+let adll=document.querySelectorAll(".id_name");
+console.dir(all);
