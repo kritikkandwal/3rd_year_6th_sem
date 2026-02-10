9235e906738cf0ec4ff80cdf413d810d54082819
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-02-10 13:52:12 +0530

    Time: 2 ms (88.76%), Space: 80.7 MB (24.38%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -5,7 +5,7 @@ class Solution {
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] == nums2[j]) {
                 return nums1[i];
-            } 
+            }
             else if (nums1[i] < nums2[j]) {
                 i++;
             } 
