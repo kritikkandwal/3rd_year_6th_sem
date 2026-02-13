406b569f9b1a2c0f00d92dd6e0fa937dc1c69d8d
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-02-13 14:54:32 +0530

    Time: 0 ms (100%), Space: 42.5 MB (16.73%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         if (num == 1) return true;
-
         long left = 1;
         long right = num / 2;
 
         while (left <= right) {
             long mid = left + (right - left) / 2;
             long square = mid * mid;
-
-            if (square == num) {
+            if(square==num){
                 return true;
             }
-            else if (square < num) {   // FIXED
-                left = mid + 1;
+            else if(square<num){
+                left=mid+1;
             }
-            else {
-                right = mid - 1;
+            else{
+                right=mid-1;
             }
         }
         return false;
