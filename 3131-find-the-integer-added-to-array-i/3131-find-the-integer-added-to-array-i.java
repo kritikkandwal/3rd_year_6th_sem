d779a3f681a5c4372c7248a16482b40868066b28
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-02-16 11:53:18 +0530

    Time: 0 ms (100%), Space: 44.9 MB (77.63%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -4,10 +4,9 @@ class Solution {
         int sum2=0;
         for(int i=0;i<nums1.length;i++){
             sum1=sum1+nums1[i];
+            sum2=sum2+nums2[i];
         }
-        for(int j=0;j<nums2.length;j++){
-            sum2=sum2+nums2[j];
-        }
+    
 
         int diff=sum2-sum1;
         int divis=diff/nums1.length;
